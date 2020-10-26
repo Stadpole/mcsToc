99b98f88e8927b1b0685d990c703eab42f1b99d2
xutianjin_scc <2209495405@qq.com>
2020-10-26 08:56:18 +0800

    mcsToc-20201026

---
+++ provider-playback/src/main/java/com/example/providerplayback/ProviderPlaybackApplication.java
@@ -0,0 +1,13 @@
+package com.example.providerplayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderPlaybackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderPlaybackApplication.class, args);
+    }
+
+}
---
+++ provider-playback/src/test/java/com/example/providerplayback/ProviderPlaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.providerplayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderPlaybackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
